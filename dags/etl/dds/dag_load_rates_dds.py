563bef2f49e4d2d5c4ea66556d4e336df8f18f38
AlekseiKim <kimalex8589@gmail.com>
2025-12-24 14:26:31 +0300

    Добавление DAGs

---
+++ dags/etl/dds/dag_load_rates_dds.py
@@ -0,0 +1,112 @@
+import logging
+import pendulum
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.sensors.external_task import ExternalTaskSensor
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+# Конфигурация логирования
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+)
+
+# Конфигурация DAG
+OWNER = "kim-av"
+DAG_ID = "dag_for_loading_dds_rates"
+TAGS = ["example", "etl"]
+
+# Длинное описание
+LONG_DESCRIPTION = """
+Загрузка данных по курсу переводов денежных средств через сервис Золотая корона.
+"""
+
+# Короткое описание
+SHORT_DESCRIPTION = "Загрузка данных по курсу переводов денежных средств через сервис Золотая корона."
+
+# Аргументы по-умолчанию
+default_args = {
+    "owner": OWNER,
+    "depends_on_past": False,
+    "retries": 1,
+    "retry_delay": pendulum.duration(minutes=3),
+}
+
+
+def load_rates_to_dds(**context):
+    pg_hook = PostgresHook("my_dwh")
+    insert_sql = """
+        -- Закрываем старые версии, где курс изменился
+        UPDATE dds.korona_transfer_rates d
+        SET ts_to = o.load_ts
+        FROM ods.korona_transfer_rates o
+        WHERE d.sending_currency_id = o.sending_currency_id
+          AND d.receiving_currency_id = o.receiving_currency_id
+          AND d.ts_to = TIMESTAMP '9999-12-31'
+          AND d.exchange_rate IS DISTINCT FROM o.exchange_rate;
+        
+        -- Вставляем новые версии
+        INSERT INTO dds.korona_transfer_rates (
+            sending_currency_id,
+            receiving_currency_id,
+            ts_from,
+            exchange_rate
+        )
+        SELECT 
+            o.sending_currency_id,
+            o.receiving_currency_id,
+            o.load_ts,
+            o.exchange_rate
+        FROM ods.korona_transfer_rates o
+        LEFT JOIN dds.korona_transfer_rates d
+            ON o.sending_currency_id = d.sending_currency_id
+           AND o.receiving_currency_id = d.receiving_currency_id
+           AND d.ts_to = TIMESTAMP '9999-12-31'
+        WHERE d.rate_sk IS NULL -- либо курс отличается, если нужно
+           OR d.exchange_rate IS DISTINCT FROM o.exchange_rate;
+    """
+    try:
+        with pg_hook.get_conn() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(insert_sql)
+                logging.info("Загрузка курсов из ODS в DDS успешно выполнена.")
+    except Exception as e:
+        logging.exception("Ошибка при загрузке курсов из ODS в RAW")
+        raise RuntimeError(f"Could not insert using query: {e}")
+
+
+with DAG(
+    dag_id=DAG_ID,
+    description=SHORT_DESCRIPTION,
+    schedule_interval="0 * * * *",
+    start_date=pendulum.datetime(2025, 12, 24),
+    catchup=True,
+    default_args=default_args,
+    tags=TAGS,
+    max_active_runs=1,
+    max_active_tasks=1,
+) as dag:
+
+    external_task = ExternalTaskSensor(
+        task_id="wait_for_ods_rates_loaded",
+        external_dag_id="dag_for_loading_ods_rates",
+        external_task_id="end",
+        mode="reschedule",
+        poke_interval=60,
+        timeout=3600
+    )
+
+    load_rates_dds = PythonOperator(
+        task_id="load_rates_to_dds",
+        python_callable=load_rates_to_dds,
+        dag=dag,
+    )
+
+    end_task = EmptyOperator(
+        task_id="end",
+        dag=dag
+    )
+
+    external_task >> load_rates_dds >> end_task
---
+++ dags/etl/ods/dag_load_rates_ods.py
@@ -0,0 +1,98 @@
+import logging
+import pendulum
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.sensors.external_task import ExternalTaskSensor
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+# Конфигурация логирования
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+)
+
+# Конфигурация DAG
+OWNER = "kim-av"
+DAG_ID = "dag_for_loading_ods_rates"
+TAGS = ["example", "etl"]
+
+# Длинное описание
+LONG_DESCRIPTION = """
+Загрузка данных по курсу переводов денежных средств через сервис Золотая корона.
+"""
+
+# Короткое описание
+SHORT_DESCRIPTION = "Загрузка данных по курсу переводов денежных средств через сервис Золотая корона."
+
+# Аргументы по-умолчанию
+default_args = {
+    "owner": OWNER,
+    "depends_on_past": False,
+    "retries": 1,
+    "retry_delay": pendulum.duration(minutes=3),
+}
+
+
+def load_rates_to_ods(**context):
+    pg_hook = PostgresHook("my_dwh")
+    insert_sql = """
+        INSERT INTO ods.korona_transfer_rates AS ods
+        SELECT DISTINCT ON (sending_currency_id, receiving_currency_id)
+            sending_currency_id,
+            receiving_currency_id,
+            NOW() AS load_ts,
+            exchange_rate
+        FROM raw.korona_transfer_rates
+        ORDER BY sending_currency_id, receiving_currency_id, load_ts DESC
+        ON CONFLICT (sending_currency_id, receiving_currency_id)
+        DO UPDATE
+        SET
+            load_ts = NOW(),
+            exchange_rate = EXCLUDED.exchange_rate
+        WHERE ods.exchange_rate IS DISTINCT FROM EXCLUDED.exchange_rate;
+    """
+    try:
+        with pg_hook.get_conn() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(insert_sql)
+                logging.info("Загрузка курсов из RAW в ODS успешно выполнена.")
+    except Exception as e:
+        logging.exception("Ошибка при загрузке курсов из RAW в ODS")
+        raise RuntimeError(f"Could not insert using query: {e}")
+
+
+with DAG(
+    dag_id=DAG_ID,
+    description=SHORT_DESCRIPTION,
+    schedule_interval="0 * * * *",
+    start_date=pendulum.datetime(2025, 12, 24),
+    catchup=True,
+    default_args=default_args,
+    tags=TAGS,
+    max_active_runs=1,
+    max_active_tasks=1,
+) as dag:
+
+    external_task = ExternalTaskSensor(
+        task_id="wait_for_raw_rates_loaded",
+        external_dag_id="dag_for_loading_data_to_md_raw",
+        external_task_id="end",
+        mode="reschedule",
+        poke_interval=60,
+        timeout=3600
+    )
+
+    load_rates_ods = PythonOperator(
+        task_id="load_rates_to_ods",
+        python_callable=load_rates_to_ods,
+        dag=dag,
+    )
+
+    end_task = EmptyOperator(
+        task_id="end",
+        dag=dag
+    )
+
+    external_task >> load_rates_ods >> end_task
